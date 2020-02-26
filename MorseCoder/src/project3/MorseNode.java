170279c322a8a75d982b616d365bfa9c2dbb1c85
Scott Lancour <stlancour@gmail.com>
2020-02-26 14:33:36 -0600

    Updated MorseNode to include String code attribute.

--- MorseCoder/src/project3/MorseNode.java
+++ MorseCoder/src/project3/MorseNode.java
@@ -1,5 +1,29 @@
 package project3;
 
 public class MorseNode {
+	//fields
+	private char letter;
+	private String code;
+	private MorseNode left;
+	private MorseNode right;
+	private static final String EMPTY_STRING = " ";
+	private static final char EMPTY_CHAR = ' ';
 	
+	//constructor
+	public MorseNode() {
+		letter = EMPTY_CHAR;
+		code = EMPTY_STRING;
+	}
+	
+	//getLetter
+	public char getLetter() {
+		return letter;
+	}
+	
+	//get code from letter character
+	public String getCode() {
+		return code;
+	}
+	
+		
 }
